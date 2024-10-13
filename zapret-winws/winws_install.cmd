dbf0daf228099d5aa6c7584dfd8e13aa3e59174c
Kirill K. <katrovskykirill@gmail.com>
2024-10-14 01:47:34 +0400

    Update winws_install.cmd
    Поправил трошки.

--- zapret-winws/winws_install.cmd
+++ zapret-winws/winws_install.cmd
@@ -9,10 +9,10 @@ if %errorlevel% neq 0 (
 )
 
 set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
---filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=9 --new ^
 --filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
 --filter-tcp=443 --dpi-desync=fake,split --dpi-desync-fooling=badseq --new ^
---filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=n1 --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin"
+--filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d2"
 
 call :srvinst winws_optimized
 echo Служба успешно установлена и запущена.
